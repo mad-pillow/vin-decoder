6091412d9fbb3ccade37e8fd0d250f7f45e2c12c
mad-pillow <dmtr.schv@gmail.com>
2021-12-08 13:33:23 +0800

    feat: add main component App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import "./app.scss";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        Hello world
+        {/* <Header /> */}
+        {/* <Main /> */}
+        {/* <Variables /> */}
+      </div>
+    );
+  }  
+}
+
+export default App;
