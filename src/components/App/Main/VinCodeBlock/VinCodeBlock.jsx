2ffc5cade4970448c8445ab69a194fb20ef259eb
mad-pillow <dmtr.schv@gmail.com>
2021-12-08 23:16:02 +0800

    feat: add VIN request and VIN history logic

--- src/components/App/Main/VinCodeBlock/VinCodeBlock.jsx
+++ src/components/App/Main/VinCodeBlock/VinCodeBlock.jsx
@@ -1,23 +1,83 @@
 import './vinCodeBlock.scss';
+import React, { Component, createRef } from 'react';
 
-export default function VinCodeBlock() {
-  return (
-    <div className="vin-code__container">
+export default class VinCodeBlock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.VINInputRef = createRef();
+
+    this.state = {
+      currentVIN: "",
+      VINList: JSON.parse(localStorage.getItem("VINList")) || [undefined, undefined, undefined, undefined, undefined],
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.VINInputRef.current.focus();
+    this.VINInputRef.current.value = "";
+
+    const VINList = [...this.state.VINList];
+
+    VINList.unshift(this.state.currentVIN);
+    VINList.splice(5);
+
+    localStorage.setItem("VINList", JSON.stringify(VINList));
+    this.setState({ VINList: VINList });
+  }
+
+  handleChange = (e) => {
+    this.setState({ currentVIN: e.target.value });
+  }
+
+  handleVINListChose = (e) => {
+    const listItemId = e.target.id;
+    let VINList = this.state.VINList;
+
+    if (!listItemId || listItemId === "Empty slot") {
+      return;
+    }
+
+    VINList = VINList.filter(item => item !== listItemId);
+    VINList.unshift(listItemId);
+
+    localStorage.setItem("VINList", JSON.stringify(VINList));
+    this.setState({ VINList: VINList });
+  }
+
+  render() {
+    const VINNodesList = this.state.VINList.map((vin, index) => {
+      const VINNodeText = vin ? vin : "Empty slot";
+      
+      return <li
+        key={index}
+        id={VINNodeText}
+        className="vin-code__hystory-item"
+        onClick={this.handleVINListChose}>{VINNodeText}</li>;
+    });
+
+    return (
+      <div className="vin-code__container">
         <div className="vin-code__request-block">
-          <form className="vin-code__request-form">
-            <input type="text" className="vin-code__request-input" maxLength="17" placeholder="Enter VIN"/>
+          <form className="vin-code__request-form" onSubmit={this.handleSubmit}>
+            <input type="text"
+              className="vin-code__request-input"
+              ref={this.VINInputRef}
+              minLength="17"
+              maxLength="17"
+              placeholder="Enter VIN"
+              onChange={this.handleChange} />
             <button type="submit" className="vin-code__request-btn">Request VIN</button>
           </form>
         </div>
         <div className="vin-code__hystory-block">
           <ul className="vin-code__hystory-list">
-            <li className="vin-code__hystory-item">WVWBP7AN4DE531881</li>
-            <li className="vin-code__hystory-item">WVWBP7AN4DE531882</li>
-            <li className="vin-code__hystory-item">WVWBP7AN4DE531883</li>
-            <li className="vin-code__hystory-item">WVWBP7AN4DE531884</li>
-            <li className="vin-code__hystory-item">WVWBP7AN4DE531885</li>
+            {VINNodesList}
           </ul>
         </div>
-    </div>
-  )
+      </div>
+    )
+  }
 } 
--- src/components/Shared/InfoBlock/InfoBlock.jsx
+++ src/components/Shared/InfoBlock/InfoBlock.jsx
@@ -4,22 +4,24 @@ export default function InfoBlock() {
   return (
     <div className="info__container">
       <table className="info__table">
-        <tr className="info__row">
-          <th className="info__header">Variable</th>
-          <th className="info__header">Value</th>
-        </tr>
-        <tr className="info__row">
-          <td className="info__data">Variable 1</td>
-          <td className="info__data">Value 1</td>
-        </tr>
-        <tr className="info__row">
-          <td className="info__data">Variable 2</td>
-          <td className="info__data">Value 2</td>
-        </tr>
-        <tr className="info__row">
-          <td className="info__data">Variable 2</td>
-          <td className="info__data">Value 2</td>
-        </tr>
+        <tbody>
+          <tr className="info__row">
+            <th className="info__header">Variable</th>
+            <th className="info__header">Value</th>
+          </tr>
+          <tr className="info__row">
+            <td className="info__data">Variable 1</td>
+            <td className="info__data">Value 1</td>
+          </tr>
+          <tr className="info__row">
+            <td className="info__data">Variable 2</td>
+            <td className="info__data">Value 2</td>
+          </tr>
+          <tr className="info__row">
+            <td className="info__data">Variable 2</td>
+            <td className="info__data">Value 2</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
