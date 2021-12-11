5832f85d1868334532d704e9d57d34279927e93f
mad-pillow <dmtr.schv@gmail.com>
2021-12-11 16:59:44 +0800

    feat: add Spinner component

---
+++ src/components/Shared/Spinner/Spinner.jsx
@@ -0,0 +1,15 @@
+import "./spinner.scss";
+
+export default function Spinner() {
+  return (
+    <div className="spinner__container">
+      <div className="loadingio-spinner-dual-ball-4ss68ps5fu9">
+        <div className="ldio-np3unrjzfxn">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
