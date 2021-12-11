1ab6aa512d2a2ecd44daf034b42c8a2396135676
mad-pillow <dmtr.schv@gmail.com>
2021-12-12 01:56:39 +0800

    feat: add some adaptive stuff

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,19 +1,49 @@
 import "./header.scss";
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="header__logo-container">
-        <Link className="header__logo-link" to="/">VIN DECODER</Link>
-      </div>
-      <nav className="header__nav-container">
-        <ul className="header__nav-list">
-          <li className="header__nav-item"><Link to="/" className="header__nav-link">Main page</Link></li>
-          <li className="header__nav-item"><Link to="/variables" className="header__nav-link">Variables</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
+export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isMenuClose: true,
+    }
+  }
+
+  toggleNavMenu = () => {
+    if (this.state.isMenuClose) {
+      this.setState({ isMenuClose: false });
+    } else {
+      this.setState({ isMenuClose: true });
+    }
+  }
+
+  render() {
+    let openMenuClassName = "";
+
+    if (this.state.isMenuClose) {
+      openMenuClassName = " header__nav-container--closed";
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+
+    return (
+      <header className="header">
+        <div className="header__logo-container">
+          <Link className="header__logo-link" to="/">VIN DECODER</Link>
+        </div>
+        <nav className={`header__nav-container${openMenuClassName}`} onClick={this.toggleNavMenu}>
+          <ul className="header__nav-list">
+            <li className="header__nav-item"><Link to="/" className="header__nav-link">Main page</Link></li>
+            <li className="header__nav-item"><Link to="/variables" className="header__nav-link">Variables</Link></li>
+          </ul>
+        </nav>
+        <div className="header__menu-btn-container" onClick={this.toggleNavMenu}>
+          <span className="header__menu-btn"></span>
+        </div>
+      </header>
+    )
+  }
 }
