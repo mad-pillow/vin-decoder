5682c03f7f53c75656abead8b11f9b2660052ec7
mad-pillow <dmtr.schv@gmail.com>
2021-12-11 16:57:58 +0800

    feat: add service component VinService.js

---
+++ src/services/VinService.js
@@ -0,0 +1,44 @@
+class VinService {
+  constructor() {
+    this.API_URL = "https://vpic.nhtsa.dot.gov/api/vehicles/";
+
+    this.getCarData = this.getCarData.bind(this);
+    this.getVariables = this.getVariables.bind(this);
+  }
+
+  async getData(url) {
+    const data = await fetch(url);
+
+    if (!data.ok) {
+      throw new Error(`Cannot fetch data from ${url}. Status - ${data.status}`);
+    }
+
+    return await data.json();
+  }
+
+  async getCarData(vin) {
+    const carData = await this.getData(`${this.API_URL}decodevin/${vin}?format=json`);
+
+    let processedCarData = carData.Results.map((car) => {
+      const processedCar = { Variable: car.Variable, Value: car.Value };
+
+      return processedCar;
+    });
+
+    processedCarData = processedCarData.filter((item) => item.Value);
+
+    return { message: carData.Message, carData: processedCarData };
+  }
+
+  async getVariables() {
+    const variables = await this.getData(`${this.API_URL}getvehiclevariablelist?format=json`);
+
+    const processedVariablesData = variables.Results.map((variable) => {
+      return { Name: variable.Name, Description: variable.Description };
+    });
+
+    return { message: variables.Message, variables: processedVariablesData };
+  }
+}
+
+export default VinService;
